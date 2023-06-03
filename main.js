ff4257e4434c06f697c5045206d5f8a30bb78809
vrishan256 <88889787+vrishan256@users.noreply.github.com>
2023-06-03 13:27:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+Webcam.set({
+    width:350,
+    height:285,
+    Image_format : 'png' ,
+    png_quality : 90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach( '#camera' );
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>'
+    });
+}
+console.log('ml5 version:', ml5.version);
+classifier =
+ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json',modelLoaded);
